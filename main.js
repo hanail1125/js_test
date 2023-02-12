cefc2b6f458c448271d73737c77d80cc5d469732
hanail1125 <hanail011250@gmail.com>
2023-02-12 16:40:38 +0900

    클래스방식을 통한 상속(확장) 개념 이해하기

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
-console.log('Hello, world')
-console.log(_.camelCase('Hello, world'))
+// console.log('Hello, world')
+// console.log(_.camelCase('Hello, world'))
 
 /** ES6 Classes */
 // function User(first, last) {
@@ -13,15 +13,49 @@ console.log(_.camelCase('Hello, world'))
 //   return `${this.firstName} ${this.lastName}`
 // }
 
-class User {
-  constructor(first, last) {
-    this.firstName = first,
-    this. lastName = last
-  }
-  getFullName() {
-    return `${this.firstName} ${this.lastName}`
+// class User {
+//   constructor(first, last) {
+//     this.firstName = first,
+//     this. lastName = last
+//   }
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`
+//   }
+// }
+
+// const hsi = new User('Seungil', 'Hwang')
+// console.log(hsi.getFullName())
+
+
+/** 상속(확장) */
+class Vehicle {
+  constructor(name, wheel) {
+    this.name = name,
+    this.wheel = wheel
   }
 }
 
-const hsi = new User('Seungil', 'Hwang')
-console.log(hsi.getFullName())
+const myVehicle = new Vehicle('운송수단', 2)
+console.log(myVehicle)
+
+class Bicycle extends Vehicle {
+  constructor(name, wheel) {
+    super(name, wheel)
+  }
+}
+const myBicycle = new Bicycle('삼천리', 2)
+const daughtersBicycle = new Bicycle('세발', 3)
+console.log(myBicycle)
+console.log(daughtersBicycle)
+
+class Car extends Vehicle {
+  constructor(name, wheel, license) {
+    super(name, wheel)
+    this.license = license
+  }
+}
+
+const myCar = new Car('벤츠', 4, true)
+const daughtersCar = new Car('포르쉐', 4, false)
+console.log(myCar)
+console.log(daughtersCar)
