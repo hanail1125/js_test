843b3b5c0313d1e68b7412da312a3d79cd99ec57
hanail1125 <hanail011250@gmail.com>
2023-02-12 16:23:54 +0900

    ES6 Classes 학습

--- main.js
+++ main.js
@@ -2,3 +2,26 @@ import _ from 'lodash';
 
 console.log('Hello, world')
 console.log(_.camelCase('Hello, world'))
+
+/** ES6 Classes */
+// function User(first, last) {
+//   this.firstName = first,
+//   this.lastName = last
+// }
+
+// User.prototype.getFullName = function() {
+//   return `${this.firstName} ${this.lastName}`
+// }
+
+class User {
+  constructor(first, last) {
+    this.firstName = first,
+    this. lastName = last
+  }
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+
+const hsi = new User('Seungil', 'Hwang')
+console.log(hsi.getFullName())
