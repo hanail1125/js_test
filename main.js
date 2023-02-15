a11874b2e7ab7237aedf99067d79f9bdf76e8155
hanail1125 <hanail011250@gmail.com>
2023-02-15 21:21:26 +0900

    axios 활용

--- main.js
+++ main.js
@@ -346,18 +346,18 @@ import * as R from './getRandom'
 
 /** OMDb API 사용법 */
 // http://www.omdbapi.com/?apikey=7035c60c&s=frozen
-import axios from "axios";
+// import axios from "axios";
 
-function fetchMovies() {
-  axios
-    .get('https://www.omdbapi.com/?apikey=7035c60c&s=frozen')
-    .then((res) => {
-      console.log(res)
-      const h1El = document.querySelector('h1')
-      const imgEl = document.querySelector('img')
-      h1El.textContent = res.data.Search[0].Title
-      imgEl.src = res.data.Search[0].Poster
-    })
-}
+// function fetchMovies() {
+//   axios
+//     .get('https://www.omdbapi.com/?apikey=7035c60c&s=frozen')
+//     .then((res) => {
+//       console.log(res)
+//       const h1El = document.querySelector('h1')
+//       const imgEl = document.querySelector('img')
+//       h1El.textContent = res.data.Search[0].Title
+//       imgEl.src = res.data.Search[0].Poster
+//     })
+// }
 
-fetchMovies()
+// fetchMovies()
